64433053ed1fc87f4495e0efcb32f5c3dfcf6619
singhtushar689 <singhtushar689@gmail.com>
2023-02-06 11:07:30 +0530

    [Tushar] add: Check Whether Heads or Tails while Flipping

---
+++ BasicCoreProgramming/FlipCoin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramming
+{
+    public class FlipCoin
+    {
+        int countHead = 0;
+        int countTail = 0;
+        float headPercentage;
+        float tailPercentage;
+        public void Flip()
+        {
+            Console.WriteLine("Enter the Times you want to FlipCoin");
+            float times = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            
+            for (int i = 0; i < times; i++)
+            {
+                double check=random.NextDouble();   
+                if (check > 0.5 )
+                    countHead++;
+                else
+                    countTail++;
+            }
+            Console.WriteLine("Head Out Of Total Flips " + countHead);
+            Console.WriteLine("Tails Out Of Total Flips " + countTail);
+            headPercentage = (countHead / times) * 100;
+            Console.WriteLine(headPercentage);
+            tailPercentage = (countTail / times) * 100;
+            Console.WriteLine(tailPercentage);
+        }
+    }
+}
---
+++ BasicCoreProgramming/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. FlipCoin \n 2. Exit");
+            
+            Console.WriteLine("Enter the Option");
+            int option = Convert.ToInt32(Console.ReadLine()); 
+            
+            switch(option)
+            {
+                case 1:
+                    FlipCoin flipCoin = new FlipCoin();
+                    flipCoin.Flip();
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
